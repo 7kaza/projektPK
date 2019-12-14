a87b70feeed7bce9f3670176dc43b1fe0427a249
student <tester@tester-m>
2019-12-14 15:37:35 +0100

    wersja9

--- litery.py
+++ litery.py
@@ -1,3 +1,5 @@
-def isalpha(txt):
-    txt = "ala ma kota"
+def funkcja_isalpha(txt):
     return txt.isalpha()
+
+def funkcja_isalnum(x):
+    return x.isalnum()
--- test_stringi.py
+++ test_stringi.py
@@ -4,8 +4,15 @@ import litery
 class test_sprawdza_tekst(unittest.TestCase):
 
      def test_isalpha(self):
-        txt = litery.isalpha("ala ma kota")
-        self.assertTrue(self,txt)
+        txt = litery.funkcja_isalpha("alamakota")
+        self.assertTrue(txt)
+
+     def test_isalnum(self):
+        x = litery.funkcja_isalnum("alama2koty")
+        self.assertTrue(x)
+        x = litery.funkcja_isalnum("ala ma 2 koty")
+        self.assertFalse(x)
+
 
 
 
